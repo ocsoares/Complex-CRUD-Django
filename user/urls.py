4c957ad041d70236b379c3b961dbcf1dba7dd680
Cauã Soares <casoares016@gmail.com>
2025-11-06 20:33:53 -0300

    add CRUD features with generic Views

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 
-from user.views import UserCreateView
+from user.views import UserListCreateView, UserRetrieveUpdateDestroyView
 
 urlpatterns = [
-    path("user/", UserCreateView.as_view(), name="user_create"),
+    path("user/", UserListCreateView.as_view(), name="user_create"),
+    path(
+        "user/<str:id>",
+        UserRetrieveUpdateDestroyView.as_view(),
+        name="user_retrieve_update_and_destroy",
+    ),
 ]
--- user/views.py
+++ user/views.py
@@ -1,9 +1,30 @@
+from uuid import UUID
+
 from rest_framework import generics
+from rest_framework.exceptions import NotFound
 
 from user.models import User
 from user.serializers import UserSerializer
 
 
-class UserCreateView(generics.CreateAPIView):
-    queryset = User.objects.all()
+# Route /user with GET & POST methods
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all().order_by("created_at")
     serializer_class = UserSerializer
+
+
+# Route /user/<id> with GET, PUT, PATCH and DELETE methods
+class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all().order_by("created_at")
+    serializer_class = UserSerializer
+    lookup_field = "id"
+
+    # Default ID message error
+    def get_object(self):
+        lookup_value = self.kwargs.get(self.lookup_field)
+
+        try:
+            formated_uuid = UUID(str(lookup_value))
+            return self.queryset.get(id=formated_uuid)
+        except (ValueError, User.DoesNotExist) as err:
+            raise NotFound(detail="User not found") from err
